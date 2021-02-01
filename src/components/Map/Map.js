5723c15a0aaf71b8698c3d8c6c1dad4dd33ceff1
Tipene Hughes <tipenehughes@gmail.com>
2021-01-31 22:23:37 -0500

    Added react tooltip. Added zoomable group to map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,21 +1,56 @@
 import React from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import {
+    ComposableMap,
+    ZoomableGroup,
+    Geographies,
+    Geography,
+} from "react-simple-maps";
 import styles from "./Map.module.css";
 
 const Map = () => {
     const geoUrl =
         "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
+    const mapWidth = 800;
+    const mapHeight = 370;
     return (
         <div className={styles.map}>
-            <ComposableMap>
-                <Geographies geography={geoUrl}>
-                    {({ geographies }) =>
-                        geographies.map((geo) => (
-                            <Geography key={geo.rsmKey} geography={geo} />
-                        ))
-                    }
-                </Geographies>
+            <ComposableMap
+                width={mapWidth}
+                height={mapHeight}
+                projectionConfig={{ scale: 150 }}
+            >
+                <ZoomableGroup
+                    center={[10, 0]}
+                    translateExtent={[
+                        [0, 0],
+                        [mapWidth, mapHeight],
+                    ]}
+                >
+                    <Geographies geography={geoUrl}>
+                        {({ geographies }) =>
+                            geographies.map((geo) => (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    style={{
+                                        default: {
+                                            fill: "#D6D6DA",
+                                            outline: "none",
+                                        },
+                                        hover: {
+                                            fill: "#F53",
+                                            outline: "none",
+                                        },
+                                        pressed: {
+                                            fill: "#E42",
+                                            outline: "none",
+                                        },
+                                    }}
+                                />
+                            ))
+                        }
+                    </Geographies>
+                </ZoomableGroup>
             </ComposableMap>
         </div>
     );
