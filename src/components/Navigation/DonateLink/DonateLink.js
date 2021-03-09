f1d46b6238cf938a8c978f5ca5c3420fa013a1d7
Tipene Hughes <tipenehughes@gmail.com>
2021-03-08 20:54:47 -0500

    Added DonateLink content

--- src/components/Navigation/DonateLink/DonateLink.js
+++ src/components/Navigation/DonateLink/DonateLink.js
@@ -1,7 +1,66 @@
 import React from "react";
 
 const DonateLink = () => {
-    return <div></div>;
+    return (
+        <motion.div
+            className={styles.menu}
+            variants={menuVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            key={"menu"}
+        >
+            <div className={styles.menuContainer}>
+                <div className={styles.menuItem}>
+                    <div className={styles.imageContainer}>
+                        <img
+                            src={NavigationMenuData[menuContent].item1.img}
+                            alt=""
+                        />
+                    </div>
+                    {NavigationMenuData[menuContent].item1.h1 && (
+                        <div className={styles.textContainer}>
+                            <h3>{NavigationMenuData[menuContent].item1.h1}</h3>
+                            <p>{NavigationMenuData[menuContent].item1.p1}</p>
+                            <p>{NavigationMenuData[menuContent].item1.p2}</p>
+                        </div>
+                    )}
+                </div>
+
+                <div className={styles.menuItem}>
+                    <div className={styles.imageContainer}>
+                        <img
+                            src={NavigationMenuData[menuContent].item2.img}
+                            alt=""
+                        />
+                    </div>
+                    {NavigationMenuData[menuContent].item2.h1 && (
+                        <div className={styles.textContainer}>
+                            <h3>{NavigationMenuData[menuContent].item2.h1}</h3>
+                            <p>{NavigationMenuData[menuContent].item2.p1}</p>
+                            <p>{NavigationMenuData[menuContent].item2.p2}</p>
+                        </div>
+                    )}
+                </div>
+
+                <div className={styles.menuItem}>
+                    <div className={styles.imageContainer}>
+                        <img
+                            src={NavigationMenuData[menuContent].item3.img}
+                            alt=""
+                        />
+                    </div>
+                    {NavigationMenuData[menuContent].item3.h1 && (
+                        <div className={styles.textContainer}>
+                            <h3>{NavigationMenuData[menuContent].item3.h1}</h3>
+                            <p>{NavigationMenuData[menuContent].item3.p1}</p>
+                            <p>{NavigationMenuData[menuContent].item3.p2}</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </motion.div>
+    );
 };
 
 export default DonateLink;
